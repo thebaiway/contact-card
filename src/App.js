ce6215d2536994177604fd58e000ace0e500b7d0
Bailee <bailee.roby@gmail.com>
2023-01-13 17:45:42 -0800

    finishing up app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Info from './Components/Info.js'
+import About from './Components/About.js'
+import Interests from './Components/Interests.js'
+import Footer from './Components/Footer.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Info />
+        <About />
+        <Interests />
+        <Footer />
+      </div>
   );
 }
 
